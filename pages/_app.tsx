ad9004d714511d930ef1240ab73eddaf3925d015
Andrei Sousa <sousandrei@gmail.com>
2021-02-18 05:48:47 +0100

    🎉 Initial commit

---
+++ pages/_app.tsx
@@ -0,0 +1,8 @@
+import "../styles/normalize.css";
+import "../styles/globals.css";
+
+function Numbers({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
+
+export default Numbers;
